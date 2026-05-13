681bb2baa8eb898c771686105b2a8bf92da6cf16
juliagbarros <juliag1barros@gmail.com>
2026-05-13 18:57:38 -0300

    JS funcionando

--- script.js
+++ script.js
@@ -1,8 +1,105 @@
-let pontos_grifinoria
-let pontos_sonserina
-let pontos_lufa_lufa
-let pontos_corvinal
+let pontos_grifinoria =0
+let pontos_sonserina =0
+let pontos_lufa_lufa =0
+let pontos_corvinal =0
+let data_atual=new Date()
 
 function troca_pagina(){
    window.location.href="teste.html"
 }
+
+function escolha_casa(){
+
+   // Lógica p pergunta 1: (com peso 2 pra tentar evitar empate entre casas)
+   if (document.getElementById("coragem").checked){
+      pontos_grifinoria+=2
+   }
+   else if (document.getElementById("inteligencia").checked){
+      pontos_corvinal+=2
+   }
+   else if (document.getElementById("lealdade").checked){
+      pontos_lufa_lufa+=2
+   }
+   else if(document.getElementById("ambicao").checked){
+      pontos_sonserina+=2
+   }
+
+   //Lógica p perguna 2:
+   if(document.getElementById("decide").checked){
+      pontos_grifinoria++
+   }
+   else if(document.getElementById("planeja").checked){
+      pontos_corvinal++
+   }
+   else if(document.getElementById("escuta").checked){
+      pontos_lufa_lufa++
+   }
+   else if(document.getElementById("arquiteta").checked){
+      pontos_sonserina++
+   }
+
+   //Lógica p terceira pergunta:
+   if(document.getElementById("desafios").checked){
+      pontos_grifinoria+=2
+   }
+   else if(document.getElementById("aprendizado").checked){
+      pontos_corvinal+=2
+   }
+   else if(document.getElementById("ajudar").checked){
+      pontos_lufa_lufa+=2
+   }
+   else if(document.getElementById("reconhecimento").checked){
+      pontos_sonserina+=2
+   }
+
+   //Lógica p quarta pergunta:
+   if(document.getElementById("destemido").checked){
+      pontos_grifinoria++
+   }
+   else if(document.getElementById("analista").checked){
+      pontos_corvinal++
+   }
+   else if (document.getElementById("pedir").checked){
+      pontos_lufa_lufa++
+   }
+   else if(document.getElementById("independente").checked){
+      pontos_sonserina++
+   }
+
+   //Lógica p ultima pergunta:
+   if(document.getElementById("aventuras").checked){
+      pontos_grifinoria++
+   }
+   else if(document.getElementById("biblioteca").checked){
+      pontos_corvinal++
+   }
+   else if(document.getElementById("amigos").checked){
+      pontos_lufa_lufa++
+   }
+   else if(document.getElementById("luxo").checked){
+      pontos_sonserina++
+   }
+
+   //resultados:
+   if (pontos_grifinoria> pontos_corvinal && pontos_grifinoria>pontos_lufa_lufa && pontos_grifinoria >pontos_sonserina){
+       window.location.href="grifinoria.html"
+   }
+   else if(pontos_corvinal> pontos_grifinoria && pontos_corvinal>pontos_lufa_lufa && pontos_corvinal >pontos_sonserina){
+       window.location.href="corvinal.html"
+   }
+   else if (pontos_lufa_lufa> pontos_grifinoria && pontos_lufa_lufa>pontos_corvinal && pontos_lufa_lufa >pontos_sonserina){
+       window.location.href="lufa.html"
+   }
+   else if (pontos_sonserina> pontos_grifinoria && pontos_sonserina>pontos_corvinal && pontos_sonserina >pontos_lufa_lufa){
+       window.location.href="sonserina.html"
+   }
+}
+
+function refazer_teste(){
+      window.location.href="teste.html"
+}
+
+function mostra_data(){
+   document.getElementById("data").innerHTML =data_atual.toLocaleDateString()
+}
+mostra_data()
